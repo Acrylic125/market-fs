28c9ece40a35c4b62ef1850b622e2df792004c02
Acrylic125 <acrylic125email@gmail.com>
2021-08-12 21:11:28 +0800

    Express

---
+++ market-backend/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use((req, resp, next) => {
+    console.log("Test!", req.url, req.protocol);
+    next();
+})
+
+app.get('/', (req, resp) => {
+    resp.json({
+        name: "ABC",
+        id: 1,
+        age: 16,
+        gender: 'F',
+        scheme: {
+            type: 'LITE',
+            cost: 1000
+        }
+    })
+});
+ 
+ app.post('/hello', function(req, res){
+    res.send("You just called the post method at '/hello'!\n");
+ });
+
+app.listen(PORT, () => 
+    console.log(`Running on port ${PORT}`));
