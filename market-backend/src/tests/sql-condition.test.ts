33badd01d1587002a24144fbbfb0291a942d65c6
Acrylic125 <acrylic125email@gmail.com>
2021-08-18 09:14:35 +0800

    Tests

--- market-backend/src/tests/sql-condition.test.ts
+++ market-backend/src/tests/sql-condition.test.ts
@@ -1,16 +1,46 @@
-import {NOT_CONDITION_OPTION, NOT_MODIFIER, SQLConditionGroup } from "../db/sql/condition/sql-condition";
-import { groupCondition, ifEqualTo, ifGreaterThan } from "../db/sql/condition/sql-condition-functions";
+import { NOT_CONDITION_OPTION, SQLConditionType } from "../db/sql/condition/sql-condition";
+import { groupCondition, ifEqualTo, ifGreaterThan, ifGreaterThanOrEqualTo, ifSmallerThan, ifSmallerThanOrEqualTo } from "../db/sql/condition/sql-condition-functions";
 
-test(`should SQL condition equal to '(NOT balance > 420 AND username = 'postgres' OR (username = 'alternate' AND (accessto = true OR city = 'New York')))'`, () => {
-    expect(
-        groupCondition(ifGreaterThan('balance', 420, NOT_CONDITION_OPTION))
-            .and(ifEqualTo('username', `postgres`))
-            .or(groupCondition(ifEqualTo('username', 'alternate'))
-                .and(groupCondition(ifEqualTo('accessto', true))
-                    .or(ifEqualTo('city', 'New York'))
-                )
+function testSQLCondition(sqlCondition: SQLConditionType, desiredSQL: string) {
+    test(`should SQL condition match '${desiredSQL}'`, () => 
+        expect(sqlCondition.toSQLString()).toBe(desiredSQL))
+}
+
+testSQLCondition(
+    groupCondition(ifGreaterThan('balance', 420, NOT_CONDITION_OPTION))
+        .and(ifEqualTo('username', `postgres`))
+        .or(groupCondition(ifEqualTo('username', 'alternate'))
+            .and(groupCondition(ifEqualTo('accessto', true))
+                .or(ifEqualTo('city', 'New York'))
             )
-        .toSQLString()
-    ).toBe(`(NOT balance > 420 AND username = 'postgres' OR (username = 'alternate' AND (accessto = true OR city = 'New York')))`);
-});
+        ), `(NOT balance > 420 AND username = 'postgres' OR (username = 'alternate' AND (accessto = true OR city = 'New York')))`);
 
+testSQLCondition(
+    ifEqualTo('balance', 420),
+    'balance = 420'
+);
+
+testSQLCondition(
+    ifGreaterThan('balance', 420),
+    'balance > 420'
+);
+
+testSQLCondition(
+    ifGreaterThanOrEqualTo('balance', 420),
+    'balance >= 420'
+);
+
+testSQLCondition(
+    ifSmallerThan('balance', 420),
+    'balance < 420'
+);
+
+testSQLCondition(
+    ifSmallerThanOrEqualTo('balance', 420),
+    'balance <= 420'
+);
+
+testSQLCondition(
+    ifGreaterThan('balance', 69, NOT_CONDITION_OPTION),
+    'NOT balance > 69'
+);
