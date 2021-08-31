c942c0ef3c69c5d1907c702236b2b652d1ab8cbd
Acrylic125 <acrylic125email@gmail.com>
2021-08-31 20:55:07 +0800

    Some changes

--- market-backend/src/db/postgres.ts
+++ market-backend/src/db/postgres.ts
@@ -1,10 +1,5 @@
 import logger from '../utils/logger';
-import { createSequelize } from './db-loader';
+console.log("Test 1");
+import connection from './db-loader';
 
-const sequelize = createSequelize();
-
-sequelize.sync().then(() => {
-    logger.success("Sequelize Successfully Loaded!");
-}).catch((err) => logger.error(err));
-
-export default sequelize;
+console.log("Test 2 ", connection);
