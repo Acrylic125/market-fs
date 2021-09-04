083fecd412577252e909a18d50ee94ae497ad530
Acrylic125 <acrylic125email@gmail.com>
2021-09-04 15:57:46 +0800

    Finally fixed issue with Post request (Resolved with enabling extended in url encoded and json middleware)

--- market-backend/src/routes/route-gate.ts
+++ market-backend/src/routes/route-gate.ts
@@ -2,7 +2,8 @@ import express from "express";
 import userRouter from "./users-route";
 var app = express();
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/user', userRouter);
 
 export default app;
--- market-backend/src/routes/users-route.ts
+++ market-backend/src/routes/users-route.ts
@@ -1,17 +1,19 @@
 import { Router } from "express";
+import userRepository from "../user/user-repo";
 
 const userRouter = Router();
 
 userRouter.route('/:id')
     .get((request, response) => {
-        response.status(200).send
+        
         response.send(`Get ${request.params.id}`);
     })
     .delete((request, response) => {
         response.send(`Delete ${request.params.id}`);
     })
     .post((request, response) => {
-        response.send(`Post ${request.params.id}`);
+        console.log(request.body);
+        response.send(`Post ${request.params.id} ${JSON.stringify(request.body, null, 4)}`);
     });
 
 export default userRouter;
