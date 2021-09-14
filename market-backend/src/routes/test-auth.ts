5bd5236f0a6f6848f497866bd7e4bc4e1ebf0ac5
Acrylic125 <acrylic125email@gmail.com>
2021-09-14 21:08:46 +0800

    Preparing for testing

--- market-backend/src/routes/test-auth.ts
+++ market-backend/src/routes/test-auth.ts
@@ -2,6 +2,11 @@ import express from 'express';
 import session from 'express-session';
 import { Strategy } from 'passport-local';
 import passport from 'passport';
+import { findOneByUsername } from '../user/user-repo';
+import { verifyPassword } from '../user/password';
+
+// https://www.youtube.com/watch?v=jXez3EsYljc
+// Testing passport JS
 
 const app = express();
 app.use(express.json());
@@ -17,8 +22,22 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.use(new Strategy((username, password, done) => {
-    
-}))
+passport.use(new Strategy(async (username, password, done) => {
+    var user = await findOneByUsername(username);
+    try {
+        return done(null,
+                    (!user && await verifyPassword(user!.password, password)) ? user : false);
+    } catch (err) {
+        return done(err, false);
+    }
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, (user as any).username);
+});
+
+passport.deserializeUser((id, done) => {
+    done(null, { id });
+});
 
 export default app;
