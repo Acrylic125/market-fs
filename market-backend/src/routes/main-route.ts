d85ea47863761cc9211f94ffbc03c943bb375575
Acrylic125 <acrylic125email@gmail.com>
2021-09-15 20:40:50 +0800

    Save

--- market-backend/src/routes/main-route.ts
+++ market-backend/src/routes/main-route.ts
@@ -1,33 +1,24 @@
 import express from "express";
-import userRouter from "./users-route";
-import cookieParser from 'cookie-parser';
-// import { PGStore } from 'connect-pg-simple';
-// import { createPool } from "../db/db";
-import authRouter from "./auth-route";
+import userRouter from "./user-route";
+import session from 'express-session';
 import passport from "passport";
 
-var app = express();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(session({
-//     secret: "TempSecret",
-//     store: new PGStore({
-//         pool: createPool(),
-//         tableName: "sessions"
-//     }),
-//     saveUninitialized: false,
-//     resave: true,
-//     cookie: {
-//         secure: false, // Temp
-//         signed: true,
-//         sameSite: 'strict'
-//     }
-// }));
-app.use(cookieParser());
+app.use(session({
+    secret: "secret",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        signed: true
+    }
+}));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use('/user', userRouter);
-app.use('/auth', authRouter);
+
+// Use routers
+app.use(userRouter);
 
 export default app;
--- market-backend/src/routes/user-route.ts
+++ market-backend/src/routes/user-route.ts
@@ -1,15 +1,50 @@
 import { Router } from "express";
+import User, { CannotParseDataAsUserError } from "../user/user";
+import { createUser, isEmailTaken, isUsernameTaken } from "../user/user-repo";
+import { jsonResponseError } from "./route-utils";
+import passport from 'passport';
 
-const userRoute = Router();
-userRoute.route("/:id")
+const userRouter = Router();
+
+// Basic methods
+userRouter.route("/:id")
     .get((req, res) => {
-
+        
     });
 
-userRoute.post("/signup", (req, res, next) => {
-
+// Signing up
+userRouter.post("/signup", async (req, res, next) => {
+    try {
+        // Tries to parse the given request body.
+        var user = await User.parseFromRequestData(req.body);
+        // Checks if the username or the email is taken.
+        if (await isUsernameTaken(user.username) || await isEmailTaken(user.email)) {
+            res.status(400)
+                .json(jsonResponseError("Username or Email Taken"));
+        } else {
+            // Creates the user in the db if nothing fails.
+            createUser(user);
+            res.status(201).json(user);
+        }
+    } catch (err) {
+        // Catches an error 
+        // Checks if the error is a CannotParseDataAsUserError 
+        // Happens when the data provided is invalid.
+        if (err instanceof CannotParseDataAsUserError) {
+            res.status(400)
+                .json(jsonResponseError("Cannot parse data"));
+        } else {
+            res.status(500)
+                .json(jsonResponseError("Unknown Error"));
+        }
+        next(err);
+    } 
 });
 
-userRoute.post("/login", (req, res, next) => {
-
+// Logging in and authentication.
+userRouter.post("/login", passport.authenticate('local'), (req, res, next) => {
+    res.status(306)
+       .redirect("/");
 });
+
+export default userRouter;
