70cbcefd467c745f2e8146204a84e34a6e52d00a
Acrylic125 <acrylic125email@gmail.com>
2022-03-10 18:04:15 +0800

    Lib SQLException class names

--- market-fs-backend/User/src/main/java/com/acrylic/controller/UserControllerExceptionHandlers.java
+++ market-fs-backend/User/src/main/java/com/acrylic/controller/UserControllerExceptionHandlers.java
@@ -28,7 +28,7 @@ public record UserControllerExceptionHandlers(DataSource dataSource) {
         ArrayUtils.contains(getErrorCodes().getDuplicateKeyCodes(), 3);
 
         AppResponseFactory.getInstance().createDefaultErrorResponse();
-
+        
 //        if (ex.getMostSpecificCause().getClass().getName().equals("org.postgresql.util.PSQLException") &&
 //                ((SQLException) ex.getMostSpecificCause()).getSQLState().equals("23505"))
 //            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "There is already a user with the same username or email.");
