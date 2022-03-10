250f51ed45c4ab90d56438e17633e7eb529fa49d
Acrylic125 <acrylic125email@gmail.com>
2022-03-10 18:13:13 +0800

    Exception handling sample

--- market-fs-backend/User/src/main/java/com/acrylic/controller/UserControllerExceptionHandlers.java
+++ market-fs-backend/User/src/main/java/com/acrylic/controller/UserControllerExceptionHandlers.java
@@ -1,6 +1,5 @@
 package com.acrylic.controller;
 
-import com.acrylic.entity.User;
 import com.acrylic.response.AppResponseFactory;
 import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -10,11 +9,9 @@ import org.springframework.jdbc.support.SQLErrorCodes;
 import org.springframework.jdbc.support.SQLErrorCodesFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
-import java.util.Arrays;
 
 @ControllerAdvice(assignableTypes = UserController.class)
 public record UserControllerExceptionHandlers(DataSource dataSource) {
@@ -24,18 +21,22 @@ public record UserControllerExceptionHandlers(DataSource dataSource) {
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
-        ArrayUtils.contains(getErrorCodes().getDuplicateKeyCodes(), 3);
+    public ResponseEntity<Object> handleDataIntegrityViolationException(final DataIntegrityViolationException ex) {
+        final Throwable cause = ex.getMostSpecificCause();
+        final AppResponseFactory responseFactory = AppResponseFactory.getInstance();
+        if (cause instanceof SQLException) {
+            final String state = ((SQLException) cause).getSQLState();
+            final SQLErrorCodes errorCodes = getErrorCodes();
 
-        AppResponseFactory.getInstance().createDefaultErrorResponse();
-        
-//        if (ex.getMostSpecificCause().getClass().getName().equals("org.postgresql.util.PSQLException") &&
-//                ((SQLException) ex.getMostSpecificCause()).getSQLState().equals("23505"))
-//            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "There is already a user with the same username or email.");
-        return new ResponseEntity<>(
-                ex.getMostSpecificCause().getClass().getName(),
-                HttpStatus.UNPROCESSABLE_ENTITY
-        );
+            // Check for duplicate error.
+            if (ArrayUtils.contains(errorCodes.getDuplicateKeyCodes(), state)) {
+                return new ResponseEntity<>(
+                        responseFactory.createSimpleResponse("There is already a user with the same username or email."),
+                        HttpStatus.UNPROCESSABLE_ENTITY
+                );
+            }
+        }
+        return responseFactory.createDefaultErrorResponse();
     }
 
 }
