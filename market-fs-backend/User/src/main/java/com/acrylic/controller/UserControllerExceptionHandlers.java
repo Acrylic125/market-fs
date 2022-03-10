83a3c8316211b29dcfa59ccc2a277a7a0d9b84a7
Acrylic125 <acrylic125email@gmail.com>
2022-03-10 17:48:30 +0800

    Apache Commons 3

--- market-fs-backend/User/src/main/java/com/acrylic/controller/UserControllerExceptionHandlers.java
+++ market-fs-backend/User/src/main/java/com/acrylic/controller/UserControllerExceptionHandlers.java
@@ -1,25 +1,34 @@
 package com.acrylic.controller;
 
 import com.acrylic.entity.User;
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.support.SQLErrorCodes;
+import org.springframework.jdbc.support.SQLErrorCodesFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.Arrays;
 
 @ControllerAdvice(assignableTypes = UserController.class)
-public class UserControllerExceptionHandlers {
+public record UserControllerExceptionHandlers(DataSource dataSource) {
+
+    private SQLErrorCodes getErrorCodes() {
+        return SQLErrorCodesFactory.getInstance().getErrorCodes(dataSource);
+    }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
-        System.out.println(ex.getMostSpecificCause().getClass().getName());
+        ArrayUtils.contains(getErrorCodes().getDuplicateKeyCodes(), 3);
+//        if (Arrays.stream(getErrorCodes().getDuplicateKeyCodes()).anyMatch())
 //        if (ex.getMostSpecificCause().getClass().getName().equals("org.postgresql.util.PSQLException") &&
 //                ((SQLException) ex.getMostSpecificCause()).getSQLState().equals("23505"))
 //            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "There is already a user with the same username or email.");
-//
         return new ResponseEntity<>(
                 ex.getMostSpecificCause().getClass().getName(),
                 HttpStatus.UNPROCESSABLE_ENTITY
