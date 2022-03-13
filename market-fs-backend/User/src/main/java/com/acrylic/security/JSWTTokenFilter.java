e6c453bc4b2419080c96634ef01e3074c734dcfd
Acrylic125 <acrylic125email@gmail.com>
2022-03-13 18:20:39 +0800

    Using jjwt.

---
+++ market-fs-backend/User/src/main/java/com/acrylic/security/JSWTTokenFilter.java
@@ -0,0 +1,50 @@
+package com.acrylic.security;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JSWTTokenFilter extends OncePerRequestFilter {
+
+    private final static String BEARER_PREFIX = "Bearer ";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String authorizationValue = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authorizationValue == null || !authorizationValue.startsWith(BEARER_PREFIX)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String token = authorizationValue.split(" ")[1].trim();
+        if (!jwtTokenUtil.validate(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+    }
+
+    private boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            logger.error("Invalid JWT signature - {}", ex.getMessage());
+        } catch (MalformedJwtException ex) {
+            logger.error("Invalid JWT token - {}", ex.getMessage());
+        } catch (ExpiredJwtException ex) {
+            logger.error("Expired JWT token - {}", ex.getMessage());
+        } catch (UnsupportedJwtException ex) {
+            logger.error("Unsupported JWT token - {}", ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            logger.error("JWT claims string is empty - {}", ex.getMessage());
+        }
+        return false;
+    }
+}
--- market-fs-backend/User/src/main/java/com/acrylic/security/SecurityConfiguration.java
+++ market-fs-backend/User/src/main/java/com/acrylic/security/SecurityConfiguration.java
@@ -3,11 +3,14 @@ package com.acrylic.security;
 import com.acrylic.entity.User;
 import com.acrylic.repository.UserRepository;
 import com.acrylic.service.UserService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration
@@ -21,8 +24,12 @@ public class SecurityConfiguration
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(username ->
-            userService.findUserByUsername(username));
+        auth.userDetailsService(userService::findUserByUsername);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
