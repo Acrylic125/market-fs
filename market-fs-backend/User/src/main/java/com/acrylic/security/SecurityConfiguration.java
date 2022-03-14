592ed4317f7b08f073fda42c9588a13ec1b842ef
Acrylic125 <acrylic125email@gmail.com>
2022-03-14 10:55:49 +0800

    Using updated jackson core and databind.

--- market-fs-backend/User/src/main/java/com/acrylic/security/SecurityConfiguration.java
+++ market-fs-backend/User/src/main/java/com/acrylic/security/SecurityConfiguration.java
@@ -1,22 +1,36 @@
 package com.acrylic.security;
 
 import com.acrylic.service.UserService;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-@EnableWebSecurity
+import javax.servlet.http.HttpServletResponse;
+
+//@EnableWebSecurity
 public class SecurityConfiguration
         extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
+//    private final JWTFilter jwtFilter;
 
+    @Autowired
     public SecurityConfiguration(UserService userService) {
         this.userService = userService;
+//        this.jwtFilter = jwtFilter;
     }
 
     @Override
@@ -24,14 +38,64 @@ public class SecurityConfiguration
         auth.userDetailsService(userService::findUserByUsername);
     }
 
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        // Enable CORS and disable CSRF
+//        http = http.cors().and().csrf().disable()
+//                // Stateless Session
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                // Set unauthorized requests exception handler
+//                .exceptionHandling()
+//                .authenticationEntryPoint(
+//                        (request, response, ex) -> {
+//                            response.sendError(
+//                                    HttpServletResponse.SC_UNAUTHORIZED,
+//                                    ex.getMessage()
+//                            );
+//                        }
+//                )
+//                .and();
+//
+//        // Set permissions on endpoints
+//        http.authorizeRequests()
+//                // Our public endpoints
+//                .antMatchers("/api/public/**").permitAll()
+//                // Our private endpoints
+//                .anyRequest()
+//                .authenticated();
+//
+//        // Add JWT token filter
+//        http.addFilterBefore(
+//                jwtFilter,
+//                UsernamePasswordAuthenticationFilter.class
+//        );
+//    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
+//        return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // TODO configure web security
-    }
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source =
+//                new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.addAllowedOrigin("*");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter(source);
+//    }
+//
+//    @Bean
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
 
 }
